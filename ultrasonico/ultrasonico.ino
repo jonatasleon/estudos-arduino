6eeacc1acd7dc2274beea6952ab664081bb12c51
Jonatas Leon <jonatas.leon@gmail.com>
2016-09-07 23:32:12 -0300

    Add ultrasonico

---
+++ ultrasonico/ultrasonico.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Lendo dados do sensor...");
+}
+ 
+void loop()
+{
+  
+  float cmMsec, inMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
+  
+  Serial.print("Distancia em cm: ");
+  Serial.print(cmMsec);
+  Serial.print(" - Distancia em polegadas: ");
+  Serial.println(inMsec);
+  delay(1000);
+}
